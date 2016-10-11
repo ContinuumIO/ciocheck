722265dce0e755b9128f6b99404c075e9fd8bc12
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2016-10-11 17:16:40 -0400

    Update copyright

--- ciocheck/__init__.py
+++ ciocheck/__init__.py
@@ -1,27 +1,12 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Copyright 2016 Continuum Analytics, Inc.
+# Copyright © 2016 Continuum Analytics, Inc.
 #
-# May be copied and distributed freely only as part of an Anaconda or
-# Miniconda installation.
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
 # -----------------------------------------------------------------------------
 """ciocheck check/tester tool."""
 
 
-version_info = (0, 1, 0, 'dev0')
+version_info = (0, 2, 0, 'dev0')
 __version__ = '.'.join(map(str, version_info))
-
-
-CONFIGURATION_FILE = '.ciocheck'
-ENCODING_HEADER_FILE = '.cioencoding'
-COPYRIGHT_HEADER_FILE = '.ciocopyright'
-
-DEFAULT_ENCODING_HEADER = u"# -*- coding: utf-8 -*-\n"
-DEFAULT_COPYRIGHT_HEADER = u"""
-# -----------------------------------------------------------------------------
-# Copyright 2016 Continuum Analytics, Inc.
-#
-# May be copied and distributed freely only as part of an Anaconda or
-# Miniconda installation.
-# -----------------------------------------------------------------------------
-""".lstrip()
