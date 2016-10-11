828074d52d6699a33814d3208fd5316bf856ba74
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2016-10-11 17:17:24 -0400

    Add template handler

---
+++ ciocheck/templates.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © 2016 Continuum Analytics, Inc.
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Results templates."""
